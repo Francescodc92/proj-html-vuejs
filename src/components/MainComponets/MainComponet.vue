c4de38681c94e9e09cd58bf35979dfb7d0b57799
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-25 22:18:57 +0200

    add section events

--- src/components/MainComponets/MainComponet.vue
+++ src/components/MainComponets/MainComponet.vue
@@ -303,7 +303,60 @@ export default {
     </div>
   </section>
   <!--end section menu-->
-
+  <section id="events">
+    <div class="left">
+      <div class="events-list">
+        <div class="title">
+          <p>made with love</p>
+          <h2>delish pizza deals</h2>
+        </div>
+        <ul>
+          <li>
+            <div class="data">
+              <h3>02</h3>
+              <p>nov</p>
+            </div>
+            <div class="description">
+              <p>traditional napolitain pies in kyoto pizza mercato</p>
+              <span>
+                <font-awesome-icon icon="fa-solid fa-location-dot" />
+                204 E. pizzetta tommaso
+              </span>
+            </div>
+          </li>
+          <li>
+            <div class="data">
+              <h3>02</h3>
+              <p>nov</p>
+            </div>
+            <div class="description">
+              <p>traditional napolitain pies in kyoto pizza mercato</p>
+              <span>
+                <font-awesome-icon icon="fa-solid fa-location-dot" />
+                204 E. pizzetta tommaso
+              </span>
+            </div>
+          </li>
+          <li>
+            <div class="data">
+              <h3>02</h3>
+              <p>nov</p>
+            </div>
+            <div class="description">
+              <p>traditional napolitain pies in kyoto pizza mercato</p>
+              <span>
+                <font-awesome-icon icon="fa-solid fa-location-dot" />
+                204 E. pizzetta tommaso
+              </span>
+            </div>
+          </li>
+        
+        </ul>
+      </div>
+    </div>
+    <div class="right">
+    </div>
+  </section>
  </main>
  <!--end main-->
 </template>
@@ -533,7 +586,6 @@ export default {
   }
   //end section clients
   #menu{
-    min-height: 400px;
     .text-ceontainer{
       max-width: 400px;
       margin-inline: auto;
@@ -589,18 +641,95 @@ export default {
         }
         .in-sold{
           position: absolute;
-          top: 0;
-          right: 0;
+          top: 20px;
+          right: 10px;
           border-radius: 50%;
-          width: 60px;
-          height: 60px;
+          width: 40px;
+          height: 40px;
           display: flex;
           align-items: center;
           justify-content: center;
           background-color:$text-orange;
           color:$text-light;
+          text-transform: uppercase;
+          font-size: 12px;
         }
       }
     }
   }
+  //end section menu
+  #events{
+    display: flex;
+    margin-top: 50px;
+    .left,
+    .right{
+      width: 50%;      
+    }
+    .left{
+      height: 100%;
+      background-image: url(../../assets/img/cielostellato.PNG);
+      color:$text-light;
+      padding-block:40px ;
+      margin-bottom: 50px;
+      .events-list{
+        max-width:80% ;
+        margin-inline: auto;
+        .title{
+          text-transform: uppercase;
+          margin-bottom: 20px;
+          p{
+            font-size: 12px;
+            color: $text-orange;
+          }
+          h2{
+            font-size:18px ;
+          }
+        }
+        ul {
+          li {
+            padding-top:20px;
+            padding-bottom: 30px;
+            display: flex;
+            gap: 10px;
+
+            &:not(:last-child){
+              border-bottom: 1px dashed $bg-gold;
+            }
+            .data{
+              h3{
+                font-size: 30px;
+                color:$text-orange;
+                text-align: center;
+              }
+              p{
+                text-transform: uppercase;
+                font-weight: bold;
+                font-size: 12px;
+                text-align: center;
+              }
+            }
+            .description{
+              p{
+                text-transform: uppercase;
+                font-size: 14px;
+                font-weight: 600;
+              }
+              span{
+                color:$text-orange;
+                font-size: 12px;
+                text-transform: capitalize;
+              }
+            }
+          }
+        }
+      }
+    }
+    .right{
+      background-image: url(../../assets/img/h3-img-5a.jpg);
+      background-position: center;
+      background-size: cover;
+    }
+  }
+  //end section events
+
 </style>
--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import {faLocationDot, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import { faEye } from '@fortawesome/free-regular-svg-icons'
 import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library  fa-magnifying-glass*/
-library.add(faMagnifyingGlass, faEye, faFacebookF, faInstagram, faTwitter)
+library.add(faMagnifyingGlass, faEye, faFacebookF, faInstagram, faTwitter, faLocationDot)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
