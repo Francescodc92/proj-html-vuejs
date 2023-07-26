671d50077d19aad5927034bf8161f414f383298b
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-26 22:25:27 +0200

    add modal riservation

--- src/components/MainComponets/SectionReservations.vue
+++ src/components/MainComponets/SectionReservations.vue
@@ -4,12 +4,16 @@
       return {
         personReservation:'',
         dateReservation:'',
-        hourReservation:''
+        hourReservation:'',
+        modalIsOpen:false
       }
     },
     methods:{
       getReservation(){
-        alert(`hai riservato un tavolo per ${this.personReservation} ${this.personReservation > 1 ? 'persone' : 'persona'} per il giorno ${this.dateReservation} alle ${this.hourReservation}`)
+       this.modalIsOpen = true
+      },
+      closeModal(){
+        this.modalIsOpen = false
         this.personReservation = ''
         this.dateReservation = ''
         this.hourReservation = ''
@@ -23,25 +27,50 @@
       <form action="" @submit.prevent="getReservation()">
         <div class="input-wrapper">
           <label for="person-reservation"></label>
-          <input type="text" id="person-reservation" placeholder="1 Person" v-model="personReservation">
+          <input type="text" id="person-reservation" placeholder="1 Person" v-model="personReservation"  required>
           <font-awesome-icon icon="fa-solid fa-user-group" class="icon" />
         </div>
         <p>for</p>
         <div class="input-wrapper">
           <label for="date-reservation"></label>
-          <input type="text" id="date-reservation" placeholder="11/29/2020" v-model="dateReservation">
+          <input type="text" id="date-reservation" placeholder="11/29/2020" v-model="dateReservation" required>
           <font-awesome-icon icon="fa-regular fa-calendar" class="icon"  />
         </div>
         <p>at</p>
         <div class="input-wrapper">
           <label for="hour-reservation"></label>
-          <input type="text" id="hour-reservation" placeholder="7:00 pm" v-model="hourReservation">
+          <input type="text" id="hour-reservation" placeholder="7:00 pm" v-model="hourReservation" required>
           <font-awesome-icon icon="fa-regular fa-clock" class="icon"  />
         </div>
         <button>
           book a table
         </button>
       </form>
+    </div>
+    <div class="modal-reservation" v-if="modalIsOpen">
+      <div class="info">
+        <h2>Ricevuta</h2>
+        <p>hai riservato un tavolo per..</p>
+        <ul>
+          <li>
+            -
+            <span>{{ personReservation }}</span>
+             persone
+          </li>
+          <li>
+            -
+            il giorno  <span> {{ dateReservation }} </span>
+          </li>
+          <li>
+            -
+            alle ore <span>{{ hourReservation }}</span>
+          </li>
+        </ul>
+        <div class="buttons-wrapper">
+          <button class="reserv" @click="closeModal()">Conferma</button>
+          <button class="cancel" @click="closeModal()">Annulla</button>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -108,6 +137,84 @@
           cursor: pointer;
           &:hover{
             background-color: $brown;
+
+          }
+        }
+      }
+    }
+    .modal-reservation{
+      width: 100%;
+      height: 100vh;
+      background-image: url(../../assets/img/cielostellato.PNG);
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      position: fixed;
+      z-index: 200;
+      top: 0;
+      left: 0;
+      display: flex;
+      align-items:center ;
+      justify-content: center;
+      .info{
+        width: 300px;
+        height: 70%;
+        background-color: $bg-light-4;
+        border-radius:20px ;
+        h2{
+          text-align: center;
+          padding-block:20px;
+          color: $text-gold;
+          text-transform: uppercase;
+        }
+        p{
+          text-transform: uppercase;
+          font-size: 12px;
+          color: $text-orange;
+          text-align: center;
+          margin-bottom:20px ;
+        }
+        ul{
+          padding-left: 20px;
+          li{
+            font-size: 20px;
+            text-transform: capitalize;
+            margin-bottom: 10px;
+            span{
+              color: $text-orange;
+              font-weight: bold;
+            }
+          }
+        }
+        .buttons-wrapper{
+          margin-top: 50px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 20px;
+
+          button{
+            transition: background-color 200ms ease-in-out,
+                        color 200ms ease-in-out;
+            border: none;
+            padding: 10px 15px;
+            text-transform: uppercase;
+            cursor: pointer;
+            font-weight: 600;
+            &.cancel{
+              color: $text-light;
+              background-color: $brown-2;
+              &:hover{
+                background-color: $brown;
+              }
+            }
+            &.reserv{
+              border: 1px solid $text-green;
+              &:hover{
+                background-color: $text-green;
+                color: $text-light;
+              }
+            }
 
           }
         }
